59bbb70971214c140537501be3b9b5a0aaf6f8bd
marie-cha9ne <marie.c.eliassen@gokstadakademiet.no>
2025-05-09 11:42:23 +0200

    ført inn url til crudcrud og api'et. lagt til logout mulighet i app.html

---
+++ Response/Auth.js
@@ -0,0 +1,7 @@
+//Legg ved pages og batchsize for å kontrollere antall visninger i url i variabler se GIT ??
+
+// legger inn results=20 for å ikke overbelaste crudcrud for fort
+export const AllUserUrl =`https://randomuser.me/api/?results=20`;
+
+const crudKey = "";
+export const crudCrudUrl=`https://crudcrud.com/api/${crudKey}/users`;
