e8519fb0e12d94cbb935be7165094ec86ead0818
marie-cha9ne <marie.c.eliassen@gokstadakademiet.no>
2025-05-15 18:41:22 +0200

    Lagt til video og tekst fil

--- Response/Auth.js
+++ Response/Auth.js
@@ -1,5 +1,5 @@
 export const randomUserUrl =`https://randomuser.me/api/?results=50`;
 
-const crudKey = "7a0e8142b9384e508ab74631949ca695";
+const crudKey = "65b72e0cbc9743568127b804565620f2";
 export const crudCrudUrl=`https://crudcrud.com/api/${crudKey}/users`;
 export const randomMatchUrl=`https://crudcrud.com/api/${crudKey}/matches`;
